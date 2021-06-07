890773bae668474cb9a25719c9348b388973a068
inaridiy <inainainariinainari@gmail.com>
2021-06-07 19:49:43 +0900

    pixiを導入

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,42 @@
-export const SayHello = (): string => {
-  return 'Hello! TypeScript!';
-};
+import * as PIXI from 'pixi.js';
 
-console.log(SayHello());
+const app = new PIXI.Application({
+  width: 800,
+  height: 600,
+  backgroundColor: 0x1099bb,
+  resolution: window.devicePixelRatio || 1,
+});
+document.body.appendChild(app.view);
+
+const container = new PIXI.Container();
+
+app.stage.addChild(container);
+
+// Create a new texture
+const texture = PIXI.Texture.from(
+  'https://www.google.com/url?sa=i&url=https%3A%2F%2Fferret-plus.com%2F6845&psig=AOvVaw2ElI2htKf79ovA1dWwRAmf&ust=1623148708780000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPDG7qOqhfECFQAAAAAdAAAAABAD'
+);
+
+// Create a 5x5 grid of bunnies
+for (let i = 0; i < 25; i++) {
+  const bunny = new PIXI.Sprite(texture);
+  bunny.anchor.set(0.5);
+  bunny.x = (i % 5) * 40;
+  bunny.y = Math.floor(i / 5) * 40;
+  container.addChild(bunny);
+}
+
+// Move container to the center
+container.x = app.screen.width / 2;
+container.y = app.screen.height / 2;
+
+// Center bunny sprite in local container coordinates
+container.pivot.x = container.width / 2;
+container.pivot.y = container.height / 2;
+
+// Listen for animate update
+app.ticker.add((delta) => {
+  // rotate the container!
+  // use delta to create frame-independent transform
+  container.rotation -= 0.01 * delta;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
-const path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
 
-  entry: "./src/main.ts",
+  entry: './src/main.ts',
 
   module: {
     rules: [
       {
-        test: /\.(js|ts)$/,
+        test: /\.(tsx|ts)$/,
 
-        use: "ts-loader",
+        use: 'ts-loader',
       },
     ],
   },
 
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.tsx', '.js', '.json'],
   },
-
+  plugins: [new HtmlWebpackPlugin()],
+  target: ['web', 'es5'],
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
     compress: true,
   },
 };
